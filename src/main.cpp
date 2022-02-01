78723b305c6a0660e9de44afb4de1973764c410e
lucaskenp <lucasmonteirolima17@gmail.com>
2022-01-31 22:26:48 -0300

    ajuste

--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,99 @@
 #include <Arduino.h>
 
-#define pin2 34
-#define pin3 35
-// Variável Volatile devido ao uso em mais de uma fonte de interrupção.
-// O compilado armazena na memória RAM e não em um registrador.
 volatile int long counterAB = 0;
+volatile int long counterABatual = 0;
+
 uint8_t dir = 0;
+#define botao1 2
+#define botao2 3
+
+void a0()
+{
+  if (digitalRead(botao2) == LOW)
+  {
+    dir = 1;
+    delay(100);
+  }
+  else
+  {
+    dir = 0;
+    delay(100);
+  }
+  if (digitalRead(botao2) == HIGH && digitalRead(botao1) == LOW)
+  {
+    counterAB++;
+    delay(100);
+  }
+  else
+  {
+    counterAB--;
+    delay(100);
+  }
+
+  if (digitalRead(botao2) == LOW && digitalRead(botao1) == HIGH)
+  {
+    counterAB++;
+    delay(100);
+  }
+  else
+  {
+    counterAB--;
+    delay(100);
+  }
+}
+void a1()
+{
+
+  // Determina qual o sentido de giro do encoder para o contador de voltas
+  if (digitalRead(botao1) == HIGH)
+  {
+    dir = 1;
+    delay(100);
+  }
+  else
+  {
+    dir = 0;
+    delay(100);
+  }
+  // Incrementa ou decrementa o contador de acordo com a condição do sinal no canal A
+  if (digitalRead(botao1) == LOW && digitalRead(botao2) == HIGH)
+  {
+    counterAB--;
+    delay(100);
+  }
+  else
+  {
+    counterAB++;
+    delay(100);
+  }
+
+  if (digitalRead(botao1) == HIGH && digitalRead(botao2) == LOW)
+  {
+    counterAB--;
+    delay(100);
+  }
+  else
+  {
+    counterAB++;
+    delay(100);
+  }
+}
 
 void setup()
 {
   Serial.begin(115200);
+  pinMode(botao1, INPUT_PULLUP);
+  pinMode(botao2, INPUT_PULLUP);
 
-  // AttachInterrupt0, digital Pin 2, Sinal A
-  // Ativa interrupção em qualquer mudança do sinal
-  attachInterrupt(digitalPinToInterrupt(pin2), ai0, CHANGE);
-
-  // AttachInterrupt1, digital pin 3, Sinal B
-  // Ativa interrupção em qualquer mudança do sinal
-  attachInterrupt(digitalPinToInterrupt(pin3), ai1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(botao1), a0, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(botao2), a1, CHANGE);
 }
 
 void loop()
 {
-  Serial.print("Pulsos: ");
-  Serial.println(counterAB);
-}
-
-// AttachInterrupt0, digital Pin 2, Sinal A - Qualquer mudança de borda (CHANGE)
-void ai0()
-{
-
-  if (digitalRead(pin3) == LOW)
-  {
-    dir = 1;
-  }
-  else
-  {
-    dir = 0;
-  }
-
-  // Incrementa ou decrementa o contador de acordo com a condição do sinal no canal B
-  if (digitalRead(pin3) == HIGH && digitalRead(pin2) == LOW)
-  {
-    counterAB++;
-  }
-  else
-  {
-    counterAB--;
-  }
-
-  if (digitalRead(pin3) == LOW && digitalRead(pin2) == HIGH)
-  {
-    counterAB++;
-  }
-  else
-  {
-    counterAB--;
-  }
-}
-
-// AttachInterrupt1, digital Pin 3, Sinal B - Qualquer mudança de borda (CHANGE)
-void ai1()
-{
-
-  // Determina qual o sentido de giro do encoder para o contador de voltas
-  if (digitalRead(pin2) == HIGH)
-  {
-    dir = 1;
-  }
-  else
-  {
-    dir = 0;
-  }
-  // Incrementa ou decrementa o contador de acordo com a condição do sinal no canal A
-  if (digitalRead(pin2) == LOW && digitalRead(pin3) == HIGH)
-  {
-    counterAB--;
-  }
-  else
-  {
-    counterAB++;
-  }
-
-  if (digitalRead(pin2) == HIGH && digitalRead(pin3) == LOW)
-  {
-    counterAB--;
-  }
-  else
-  {
-    counterAB++;
-  }
+  Serial.print("Pulso: ");
+  Serial.print(counterAB);
+  Serial.print(" ----- Dir: ");
+  Serial.println(dir);
+  delay(500);
 }
